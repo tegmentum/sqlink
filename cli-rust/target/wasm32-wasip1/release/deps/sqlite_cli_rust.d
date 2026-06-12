751a8d460627c0c5348cf8c03cdcdbd878807f27
Zachary Whitley <zachary.whitley@tegmentum.ai>
2026-06-12 11:12:47 -0400

    chore(cli-rust): gitignore target/, untrack accidentally committed cache

--- cli-rust/target/wasm32-wasip1/release/deps/sqlite_cli_rust.d
+++
@@ -1,6 +0,0 @@
-/Users/zacharywhitley/git/sqlite-wasm/cli-rust/target/wasm32-wasip1/release/deps/sqlite_cli_rust.d: src/lib.rs src/bindings.rs
-
-/Users/zacharywhitley/git/sqlite-wasm/cli-rust/target/wasm32-wasip1/release/deps/sqlite_cli_rust.wasm: src/lib.rs src/bindings.rs
-
-src/lib.rs:
-src/bindings.rs:
